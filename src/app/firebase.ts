e50c9c1fc93bef24b6abf50663b5290517ee79fd
dastudillo <donnovan.astudillo@gmail.com>
2023-12-26 12:36:14 -0300

    v2.0

---
+++ src/app/firebase.ts
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCBpdKNpIjiXbrmI6HAnDTbdowHtjEBFqo",
+    authDomain: "login-2-25c24.firebaseapp.com",
+    databaseURL: "https://login-2-25c24.firebaseio.com",
+    projectId: "login-2-25c24",
+    storageBucket: "login-2-25c24.appspot.com",
+    messagingSenderId: "970631836542",
+    appId: "1:970631836542:web:998ee73f0c69005ebcb92f"
+  };
+
+  export default firebaseConfig;
